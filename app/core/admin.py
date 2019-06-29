eac0c178d3fe3b9c8187f330737402fd0a02605c
ahmet.avci <ahmet.avci@udemy.com>
2019-06-29 17:15:29 +0300

    travis-ci check

--- app/core/admin.py
+++ app/core/admin.py
@@ -7,12 +7,12 @@ from core import models
 
 class UserAdmin(BaseUserAdmin):
     ordering = ['id']
-    list_display = ['email','name']
+    list_display = ['email', 'name']
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
         (_('Personal Info'), {'fields': ('name',)}),
         (
-        _   ('Permissions'),
+            _('Permissions'),
             {
                 'fields': (
                     'is_active',
@@ -29,5 +29,6 @@ class UserAdmin(BaseUserAdmin):
             'fields': ('email', 'password1', 'password2')
         }),
     )
+
 
 admin.site.register(models.User, UserAdmin)
--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -2,6 +2,7 @@ from django.test import TestCase, Client
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 
+
 class AdminSiteTests(TestCase):
 
     def setUp(self):
@@ -22,13 +23,12 @@ class AdminSiteTests(TestCase):
         url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
 
-        self.assertContains(res,self.user.name)
-        self.assertContains(res,self.user.email)
-
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
 
     def test_user_change_page(self):
         """TEst that the user edit page works"""
-        url = reverse('admin:core_user_change',args=[self.user.id])
+        url = reverse('admin:core_user_change', args=[self.user.id])
         res = self.client.get(url)
 
         self.assertEqual(res.status_code, 200)
@@ -36,8 +36,6 @@ class AdminSiteTests(TestCase):
     def test_create_user_page(self):
         """Test that the create user page works"""
         url = reverse('admin:core_user_add')
-        res =self.client.get(url)
+        res = self.client.get(url)
 
         self.assertEqual(res.status_code, 200)
-
-        
