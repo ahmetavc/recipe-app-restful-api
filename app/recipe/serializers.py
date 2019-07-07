4b3c6710cd48057e3701ff6aeee311d5b113f9a8
avci <ahmetabdullahavci07@gmail.com>
2019-07-07 17:43:59 +0300

    image upload feature is added

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -37,7 +37,7 @@ class RecipeSerializer(serializers.ModelSerializer):
         fields = (
             'id', 'title', 'ingredients', 'tags', 'time_minutes', 'price',
             'link',
-         )
+        )
         read_only_fields = ('id',)
 
 
@@ -45,3 +45,12 @@ class RecipeDetailSerializer(RecipeSerializer):
     """Serialize recipe detail"""
     ingredients = IngredientSerializer(many=True, read_only=True)
     tags = TagSerializer(many=True, read_only=True)
+
+
+class RecipeImageSerializer(serializers.ModelSerializer):
+    """Serializer for uploading images to recipe"""
+
+    class Meta:
+        model = Recipe
+        fields = ('id', 'image')
+        read_only_fields = ('id',)
